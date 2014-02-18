e242944ebca595d9231b883d74cc195e32a0046a
Bitqualize <bitqualize@gmail.com>
2014-02-18 02:01:25 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OSExchange::Application.config.secret_key_base = '17aac4087b33a4d53d5ac2c4bd77893a1e4a49e9c917fd769dff467d48f5ee9e90e19a7f9eee6301fe862f672ef07a8f600846d29493f125da9da2d70e755714'
